39116624f20bc0061eb21457e2301de96f9365c1
Roxgame95 <51464152+Roxgame95@users.noreply.github.com>
2022-04-04 00:33:43 +0000

    Update FetchRetry.js

--- FetchRetry.js
+++ FetchRetry.js
@@ -1,1 +1,13 @@
-
+async function fetchRetry(url, fetchOptions = {}) {
+    function onError(err) {
+        return AvoidManyRequests(5000).then(() => {
+            console.log("Retrying...");
+            fetchRetry(url, fetchOptions);
+        });
+    }
+    return await fetch(url, fetchOptions).catch(onError);
+}
+const AvoidManyRequests = (sleepTime = 1000) => {
+    return new Promise((resolve) => setTimeout(resolve, sleepTime));
+}
+exports.fetchRetry = fetchRetry;
